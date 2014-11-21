38a6c7bf5cbf303d67f87267869d2ba4d04d77ff
Maxi <maxi_ruiz140@hotmail.com>
2014-11-21 19:01:43 -0300

    Big Text, Picture and List Notification

--- src/com/example/myfirstapp/InitActivity.java
+++ src/com/example/myfirstapp/InitActivity.java
@@ -5,6 +5,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -67,12 +69,18 @@ public class InitActivity extends Activity {
 		Intent i = new Intent(this, NotificationActivity.class);
 		i.putExtra("notificationID", notificationID);
 		PendingIntent pIntent = PendingIntent.getActivity(this, 0, i, 0);
+		Uri sonido = RingtoneManager.getDefaultUri(Notification.DEFAULT_SOUND);
 		
 		Notification noti = new Notification.Builder(this)
+							.setWhen(System.currentTimeMillis())
 							.setTicker("Tienes una Notificación")
 							.setContentTitle("Notificación")
 							.setContentText("Click para ver la Notificación")
+							.setContentInfo("Noti")
 							.setSmallIcon(R.drawable.ic_launcher)
+							.setSound(sonido)
+							.addAction(R.drawable.ic_launcher, "Titulo de Accion", pIntent)
+							.setVibrate(new long[] {100,250,100,500})
 							.setContentIntent(pIntent).build();
 		
 		noti.flags = Notification.FLAG_AUTO_CANCEL;
--- src/com/example/myfirstapp/NotificationActivity.java
+++ src/com/example/myfirstapp/NotificationActivity.java
@@ -1,10 +1,19 @@
 package com.example.myfirstapp;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NotificationActivity extends Activity {
 
@@ -36,4 +45,83 @@ public class NotificationActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	public void bigText(View v) {
+		Intent intent = new Intent();
+		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+		
+		NotificationCompat.BigTextStyle style = new NotificationCompat.BigTextStyle();
+		style.setSummaryText("Summary");
+		style.setBigContentTitle("Big Content Title");
+		style.bigText("Bueno, esto es un poco problematico de explicar. Cada vez que compilo este código, y si el dispositivo esta conecado, no se ven los cambios de estilo. Pero al desconectar eld ispositivo y correr la app, los cambio se ven perfectamente.");
+		
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+		builder.setAutoCancel(true);
+		builder.setTicker("Big Text");
+		builder.setContentTitle("Big Test Reduced");
+		builder.setContentText("Reduced Text");
+		builder.setSmallIcon(R.drawable.ic_launcher);
+		builder.setStyle(style);
+		
+		Notification notification = builder.setContentIntent(pIntent).build();
+		
+		notification.flags = Notification.FLAG_AUTO_CANCEL;
+		NotificationManager manager = (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
+		manager.notify(63612, notification);
+	}
+	
+	public void bigPicture(View v) {
+		Intent intent = new Intent();
+		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+		Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+		
+		NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle();
+		style.setSummaryText("Summary");
+		style.setBigContentTitle("Big Picure Title");
+		style.bigPicture(bmp);
+		
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+		builder.setAutoCancel(true);
+		builder.setTicker("Big Picture");
+		builder.setContentTitle("Big Picture Notification");
+		builder.setContentText("Text of Picture");
+		builder.setSmallIcon(R.drawable.ic_launcher);
+		builder.setStyle(style);
+		
+		Notification notification = builder.setContentIntent(pIntent).build();
+		
+		notification.flags = Notification.FLAG_AUTO_CANCEL;
+		NotificationManager manager = (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
+		manager.notify(63612, notification);
+	}
+	
+	public void inBox(View v) {
+		Intent intent = new Intent();
+		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+		Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+		
+		NotificationCompat.InboxStyle style = new NotificationCompat.InboxStyle();
+		style.setSummaryText("Summary");
+		style.setBigContentTitle("In Box Title");
+		style.addLine("Linea 1");
+		style.addLine("Linea 2");
+		style.addLine("Linea 3");
+		style.addLine("Linea 4");
+		style.addLine("Linea 5");
+		
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+		builder.setAutoCancel(true);
+		builder.setTicker("In Box Text");
+		builder.setContentTitle("In Box Notification Reduced");
+		builder.setContentText("In Box Text Reduced");
+		builder.setSmallIcon(R.drawable.ic_launcher);
+		builder.setStyle(style);
+		
+		Notification notification = builder.setContentIntent(pIntent).build();
+		
+		notification.flags = Notification.FLAG_AUTO_CANCEL;
+		NotificationManager manager = (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
+		manager.notify(63612, notification);
+	}
+
 }
