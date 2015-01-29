c0eea1b8df3405d149e8692c8e29bb9966d6604d
Maxi <maxi_ruiz140@hotmail.com>
2015-01-29 00:38:00 -0300

    Shared Content

--- src/com/example/myfirstapp/LenguajeActivity.java
+++ src/com/example/myfirstapp/LenguajeActivity.java
@@ -1,16 +1,32 @@
 package com.example.myfirstapp;
 
+import com.example.myfirstapp.R.id;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LenguajeActivity extends Activity {
 
+	EditText firstN, lastN;
+	Button save, load;
+	String name, last;
+	SharedPreferences preferences;
+	private SharedPreferences.Editor editor;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lenguaje);
+		
+		firstN = (EditText) findViewById(id.firstName);
+		lastN = (EditText) findViewById(id.lastName);
 	}
 
 	@Override
@@ -30,5 +46,35 @@ public class LenguajeActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void saveData(View v){
+		name = firstN.getText().toString();
+		last = lastN.getText().toString();
+		
+		preferences = getSharedPreferences("my_details", MODE_PRIVATE);
+		editor = preferences.edit();
+		
+		editor.putString("FKey", name);
+		editor.putString("LKey", last);
+		editor.commit();
+		
+		firstN.setText("");
+		lastN.setText("");
+		
+		Toast.makeText(getBaseContext(), "Save Successful", Toast.LENGTH_LONG).show();
+	}
+	
+	public void loadData(View v){
+		preferences = getSharedPreferences("my_details", MODE_PRIVATE);
+		String lName, fName;
+		
+		fName = preferences.getString("FKey", "");
+		lName = preferences.getString("LKey", "");
+		
+		firstN.setText(fName);
+		lastN.setText(lName);
+		
+		Toast.makeText(getBaseContext(), "Name: "+fName+" and Last Name: "+lName, Toast.LENGTH_LONG).show();
 	}
 }
